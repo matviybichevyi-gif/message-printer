293742e546be5661e50d616a8d8b829c4ac43391
Матвій <matviy.bichevyi@kgemt.org.ua>
2025-12-02 10:21:21 +0200

    Update welcome message to be more encouraging

--- MessagePrinter.java
+++ MessagePrinter.java
@@ -1,8 +1,8 @@
 public class MessagePrinter {
     
-    public static String getWelcomeMessage() {
-        return "Welcome to Git and GitHub!";
-    }
+   public static String getWelcomeMessage() {
+		return "Welcome! Learning GitHub is fun and easy!";
+	}
     
     public static String getDateMessage() {
         return "Today is: " + java.time.LocalDate.now();
