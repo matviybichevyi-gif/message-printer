f2391633b112a9258945b161d97312850a66e125
Матвій <matviy.bichevyi@kgemt.org.ua>
2025-12-02 10:32:32 +0200

    Add author information

--- MessagePrinter.java
+++ MessagePrinter.java
@@ -9,13 +9,18 @@ main
     public static String getDateMessage() {
         return "Today is: " + java.time.LocalDate.now();
     }
-    
+    public static String getAuthorMessage() {
+    return "Created by: Your Name";
+}
+
     public static void main(String[] args) {
-		System.out.println(getWelcomeMessage());
-		System.out.println(getDateMessage());
-		System.out.println("Learning remote repositories!");
-		System.out.println(getGoodbyeMessage());
-	}
+    System.out.println(getWelcomeMessage());
+    System.out.println(getDateMessage());
+    System.out.println(getAuthorMessage());
+    System.out.println("Learning remote repositories!");
+    System.out.println(getGoodbyeMessage());
+}
+
 	public static String getGoodbyeMessage() {
 		return "Thank you for using MessagePrinter!";
 	}
